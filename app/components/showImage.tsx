9a399b42b2df8ab1507f0d02c809a1805a798b97
Unashe Siwawa <anelesiwawa@gmail.com>
2023-12-06 23:48:43 +0200

    Fixes
    -- fixes

--- app/components/showImage.tsx
+++ app/components/showImage.tsx
@@ -1,47 +1,42 @@
-import { ref, getDownloadURL } from 'firebase/storage';
-import React, { useEffect, useState } from 'react'
-import { FC } from 'react';
+// import { ref, getDownloadURL } from 'firebase/storage';
+// import React, { useEffect, useState } from 'react'
+// import { FC } from 'react';
 
-import LazyLoad from 'react-lazyload';
-import { storage } from '../../firebase/clientApp';
-import { print } from '../utils/console';
+// import LazyLoad from 'react-lazyload';
+// import { storage } from '../../firebase/clientApp';
+// import { print } from '../utils/console';
 
+// interface MyProps {
+//     src: string,
+//     alt: string,
+//     style: string
+// }
 
-interface MyProps {
-    src: string,
-    alt: string,
-    style: string
-}
+// const ShowImage: FC<MyProps> = ({ src, alt, style }) => {
+//     const [url, setUrl] = useState("");
 
+//     useEffect(() => {
 
+//         const pathReference = ref(storage, src);
+//         getDownloadURL(pathReference).then((url) => {
+//             setUrl(url);
 
-const ShowImage: FC<MyProps> = ({ src, alt, style }) => {
-    const [url, setUrl] = useState("");
+//         })
+//             .catch((error) => {
+//                 // Handle any errors
+//                 console.error(error);
+//             });
 
-    useEffect(() => {
+//     }, [url])
 
-        const pathReference = ref(storage, src);
-        getDownloadURL(pathReference).then((url) => {
-            setUrl(url);
+//     return (
+//         <LazyLoad>
+//             <div className="m-auto">
+//                 <img src={url} alt={alt} className={style + ' object-cover w-full max-h-96'} />
+//             </div>
+//         </LazyLoad>
 
-        })
-            .catch((error) => {
-                // Handle any errors
-                console.error(error);
-            });
+//     )
+// };
 
-    }, [url])
-
-
-    return (
-        <LazyLoad>
-            <div className="m-auto">
-                <img src={url} alt={alt} className={style + ' object-cover w-full max-h-96'} />
-            </div>
-        </LazyLoad>
-
-    )
-};
-
-
-export default ShowImage
+// export default ShowImage
--- app/components/showVideo.tsx
+++ app/components/showVideo.tsx
@@ -1,46 +1,41 @@
-import { ref, getDownloadURL } from 'firebase/storage';
-import React, { useEffect, useState } from 'react'
-import { FC } from 'react';
-import { storage } from '../../firebase/clientApp';
-import { print } from '../utils/console';
-import ReactPlayer from 'react-player'
+// import { ref, getDownloadURL } from 'firebase/storage';
+// import React, { useEffect, useState } from 'react'
+// import { FC } from 'react';
+// import { storage } from '../../firebase/clientApp';
+// import { print } from '../utils/console';
+// import ReactPlayer from 'react-player'
 
-interface MyProps {
-    src: string,
-    alt: string,
-    style: string
-}
+// interface MyProps {
+//     src: string,
+//     alt: string,
+//     style: string
+// }
 
+// const ShowVideo: FC<MyProps> = ({ src, alt, style }) => {
+//     const [url, setUrl] = useState("");
 
+//     useEffect(() => {
 
-const ShowVideo: FC<MyProps> = ({ src, alt, style }) => {
-    const [url, setUrl] = useState("");
+//         const pathReference = ref(storage, src);
+//         getDownloadURL(pathReference).then((url) => {
+//             setUrl(url);
 
-    useEffect(() => {
+//         })
+//             .catch((error) => {
+//                 // Handle any errors
+//                 console.error(error);
+//             });
 
-        const pathReference = ref(storage, src);
-        getDownloadURL(pathReference).then((url) => {
-            setUrl(url);
+//     }, [url])
 
-        })
-            .catch((error) => {
-                // Handle any errors
-                console.error(error);
-            });
+//     return (
 
-    }, [url])
+//         <div className="m-auto">
+//             {/* <img src={url} alt={alt} className={style + ' object-cover'} /> */}
+//             <ReactPlayer url={url} controls className="w-full max-h-96" />
+//         </div>
 
+//     )
+// };
 
-    return (
-
-        <div className="m-auto">
-            {/* <img src={url} alt={alt} className={style + ' object-cover'} /> */}
-            <ReactPlayer url={url} controls className="w-full max-h-96" />
-        </div>
-
-
-    )
-};
-
-
-export default ShowVideo
+// export default ShowVideo
