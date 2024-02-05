938f5a6da5e65e9474a7f913e4f0a85b04585fb0
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-02-05 13:54:15 +0200

    app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { Step1Component } from './features/auth/register/step-1/step-1.component';
-import { Step2Component } from './features/auth/register/step-2/step-2.component';
-import { Step3Component } from './features/auth/register/step-3/step-3.component';
 
 export const routes: Routes = [
   {
@@ -24,11 +21,6 @@ export const routes: Routes = [
       import('./features/auth/register/register.component').then(
         (m) => m.RegisterComponent
       ),
-    children: [
-      { path: 'step1', component: Step1Component },
-      { path: 'step2', component: Step2Component },
-      { path: 'step3', component: Step3Component },
-    ],
   },
   { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
