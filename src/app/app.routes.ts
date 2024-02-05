2fd19cbf1464b2ba3b28990a731bb728f136cfb1
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-02-05 18:08:48 +0200

    router config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '/dashboard',
+    path: 'dashboard',
     loadComponent: () =>
       import('./dashboard/dashboard.component').then(
         (m) => m.DashboardComponent
