a3118fe11994052f28b9a5544632db575175e927
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-02-05 17:59:21 +0200

    corrected dashboard route

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -111,7 +111,7 @@ export class RegisterComponent implements OnInit, AfterViewInit {
                   });
 
                   setTimeout(() => {
-                    this.router.navigate(['/dashboard']);
+                    this.router.navigate(['/']);
                   }, 3000);
                 },
                 (error) => {
