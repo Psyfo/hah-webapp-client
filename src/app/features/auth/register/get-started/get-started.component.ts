916d2f6bc6ed8e2ccc1b2c976749284d3e7efaa8
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-02-15 09:47:13 +0200

    Refactor get-started component HTML and TypeScript files

--- src/app/features/auth/register/get-started/get-started.component.ts
+++ src/app/features/auth/register/get-started/get-started.component.ts
@@ -1,15 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-get-started',
   standalone: true,
-  imports: [],
+  imports: [ToastModule, MessagesModule, ButtonModule],
   templateUrl: './get-started.component.html',
   styleUrl: './get-started.component.css',
 })
 export class GetStartedComponent {
   router = inject(Router);
+  messageService = inject(MessagesModule);
 
   goToDashboard() {
     this.router.navigate(['/dashboard']);
