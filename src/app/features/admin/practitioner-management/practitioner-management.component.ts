3b7b8f1b8dd0e6c7d6d78aba271381fda40257e5
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-03-12 01:18:05 +0200

    Add practitioner-management component

---
+++ src/app/features/admin/practitioner-management/practitioner-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practitioner-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './practitioner-management.component.html',
+  styleUrl: './practitioner-management.component.css'
+})
+export class PractitionerManagementComponent {
+
+}
