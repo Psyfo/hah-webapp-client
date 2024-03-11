fa269dfefe153ab0ff5bce761680bcae7e3a4e3f
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-03-12 01:17:19 +0200

    Add admin component with HTML, CSS, and TypeScript files

---
+++ src/app/features/admin/admin.component.ts
@@ -0,0 +1,156 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { routerTransitionSlideUp } from 'app/core/utilities/animations';
+import { MenuItem, MessageService, PrimeIcons } from 'primeng/api';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { MessagesModule } from 'primeng/messages';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { RippleModule } from 'primeng/ripple';
+import { SidebarModule } from 'primeng/sidebar';
+import { ToastModule } from 'primeng/toast';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [
+    AvatarGroupModule,
+    AvatarModule,
+    ButtonModule,
+    CardModule,
+    CommonModule,
+    InputTextModule,
+    MenuModule,
+    MenubarModule,
+    MessagesModule,
+    PanelMenuModule,
+    RippleModule,
+    RouterModule,
+    SidebarModule,
+    ToastModule,
+  ],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css',
+  animations: [routerTransitionSlideUp],
+})
+export class AdminComponent implements OnInit {
+  messageService = inject(MessageService);
+  router = inject(Router);
+
+  navItems: MenuItem[] = [];
+  panelMenuItems: MenuItem[] = [];
+
+  sidebarVisible: boolean = true;
+
+  ngOnInit() {
+    this.navItems = [];
+
+    this.panelMenuItems = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        items: [
+          {
+            label: 'Dashboard',
+            icon: 'pi pi-fw pi-chart-bar',
+            command: () => {
+              this.router.navigate(['/admin']);
+            },
+          },
+          {
+            label: 'Projects',
+            icon: 'pi pi-fw pi-folder',
+            command: () => {},
+          },
+          {
+            label: 'Tasks',
+            icon: 'pi pi-fw pi-check-square',
+            command: () => {},
+          },
+        ],
+      },
+      {
+        label: 'Users',
+        icon: 'pi pi-fw pi-users',
+        items: [
+          {
+            label: 'Patients',
+            icon: 'pi pi-fw pi-user',
+            command: () => {
+              this.router.navigate(['/admin/patient-management']);
+            },
+          },
+          {
+            label: 'Practitioners',
+            icon: 'pi pi-fw pi-user',
+          },
+          {
+            label: 'Admins',
+            icon: 'pi pi-fw pi-cog',
+            command: () => {
+              this.router.navigate(['/admin/admin-management']);
+            },
+          },
+        ],
+      },
+      {
+        label: 'Appointments',
+        icon: 'pi pi-fw pi-calendar',
+        items: [
+          {
+            label: 'Upcoming',
+            icon: 'pi pi-fw pi-calendar-plus',
+          },
+          {
+            label: 'History',
+            icon: 'pi pi-fw pi-calendar-minus',
+          },
+        ],
+      },
+      {
+        label: 'Performance',
+        icon: 'pi pi-fw pi-chart-bar',
+        items: [
+          {
+            label: 'Analytics',
+            icon: 'pi pi-fw pi-chart-line',
+          },
+          {
+            label: 'Reports',
+            icon: 'pi pi-fw pi-file',
+          },
+        ],
+      },
+      {
+        label: 'Application',
+        icon: 'pi pi-fw pi-cog',
+        items: [
+          {
+            label: 'Settings',
+            icon: 'pi pi-fw pi-cog',
+          },
+          {
+            label: 'Help',
+            icon: 'pi pi-fw pi-question-circle',
+          },
+        ],
+      },
+    ];
+  }
+
+  openNew() {
+    console.log('New Project');
+  }
+
+  logout() {}
+
+  closeCallback(event: any) {
+    this.sidebarVisible = false;
+  }
+}
