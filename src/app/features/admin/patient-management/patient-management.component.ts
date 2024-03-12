34ff79b7473c28333dc566d57ec0f31959c3dfd5
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-03-12 12:00:51 +0200

    Refactor patient management component

--- src/app/features/admin/patient-management/patient-management.component.ts
+++ src/app/features/admin/patient-management/patient-management.component.ts
@@ -83,26 +83,16 @@ export class PatientManagementComponent implements OnInit, AfterViewInit {
     { label: 'Rejected', value: 'rejected' },
   ];
 
-  updatePatientForm!: FormGroup;
+  patientForm!: FormGroup;
   isFormSubmitted: boolean = false;
   updatingPatient: boolean = false;
+  patientDialogVisible = false;
+  isUpdate: boolean = false;
 
   ngOnInit(): void {
-    this.patientService.getPatients().subscribe(
-      (patients) => {
-        this.patients = patients;
-        console.log(this.patients);
-      },
-      (error: any) => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'An error occurred while retrieving patients.',
-        });
-      }
-    );
+    this.getPatients();
 
-    this.updatePatientForm = this.fb.group({
+    this.patientForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
@@ -115,7 +105,28 @@ export class PatientManagementComponent implements OnInit, AfterViewInit {
   }
 
   get f() {
-    return this.updatePatientForm.controls;
+    return this.patientForm.controls;
+  }
+
+  getPatients() {
+    this.patientService.getPatients().subscribe(
+      (patients) => {
+        this.patients = patients;
+        console.log(this.patients);
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Info',
+          detail: 'Patients loaded.',
+        });
+      },
+      (error: any) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'An error occurred while retrieving patients.',
+        });
+      }
+    );
   }
 
   openNew() {}
@@ -124,72 +135,98 @@ export class PatientManagementComponent implements OnInit, AfterViewInit {
     this.dt.filterGlobal($event.target.value, 'contains');
   }
 
-  updateDialogVisible = false;
+  openNewPatientDialog() {
+    this.isUpdate = false;
+    this.patientDialogVisible = true;
+  }
+
   openUpdatePatientDialog(patient: IPatient) {
-    this.updateDialogVisible = true;
-    this.updatePatientForm.patchValue({
+    this.isUpdate = true;
+    this.patientDialogVisible = true;
+
+    this.patientForm.patchValue({
       email: patient.email,
       firstName: patient.firstName,
       lastName: patient.lastName,
       verified: patient.account?.verified,
       approvalStatus: patient.account?.approvalStatus,
     });
+
     this.selectedPatient = patient;
   }
   closeUpdatePatientDialog() {
-    this.updateDialogVisible = false;
+    this.isFormSubmitted = false;
+    this.patientDialogVisible = false;
   }
 
   updatePatient() {
     this.isFormSubmitted = true;
     this.updatingPatient = true;
 
-    if (this.updatePatientForm.invalid) {
+    if (this.patientForm.invalid) {
       this.updatingPatient = false;
       return;
     }
 
-    const updatedPatient: IPatient = Object.assign({}, this.selectedPatient, {
-      email: this.updatePatientForm.value.email,
-      firstName: this.updatePatientForm.value.firstName,
-      lastName: this.updatePatientForm.value.lastName,
-      account: {
-        verified: this.updatePatientForm.value.verified,
-        approvalStatus: this.updatePatientForm.value.approvalStatus,
-      },
-    });
-    console.log('Updated Patient:', updatedPatient);
+    if (this.isUpdate) {
+      const updatedPatient: IPatient = Object.assign({}, this.selectedPatient, {
+        email: this.patientForm.value.email,
+        firstName: this.patientForm.value.firstName,
+        lastName: this.patientForm.value.lastName,
+        account: {
+          verified: this.patientForm.value.verified,
+          approvalStatus: this.patientForm.value.approvalStatus,
+        },
+      });
+      console.log('Updated Patient:', updatedPatient);
 
-    this.patientService.updatePatient(updatedPatient).subscribe(
-      (response) => {
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Success',
-          detail: 'Patient updated successfully.',
-        });
-        this.updatingPatient = false;
-        this.isFormSubmitted = false;
+      this.patientService.updatePatient(updatedPatient).subscribe(
+        (response) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Patient updated successfully.',
+          });
+          this.updatingPatient = false;
+          this.isFormSubmitted = false;
 
-        // Update the value in the local DataTable array
-        // const index = this.dt.findIndexInSelection((patient: IPatient) => {
-        //   patient.id === updatedPatient.id;
-        // });
-        // if (index !== -1) {
-        //   this.dt[index] = newValue;
-        // }
-      },
-      (error: any) => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'An error occurred while updating the patient.',
-        });
-        this.updatingPatient = false;
-        this.isFormSubmitted = false;
-      }
-    );
+          this.getPatients();
+        },
+        (error: any) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'An error occurred while updating the patient.',
+          });
+          this.updatingPatient = false;
+          this.isFormSubmitted = false;
+        }
+      );
+    } else {
+      this.patientService.createPatient(this.patientForm.value).subscribe(
+        (response) => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Patient created successfully.',
+          });
+          this.updatingPatient = false;
+          this.isFormSubmitted = false;
 
-    this.closeUpdatePatientDialog();
+          this.getPatients();
+        },
+        (error: any) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'An error occurred while creating the patient.',
+          });
+          this.updatingPatient = false;
+          this.isFormSubmitted = false;
+        }
+      );
+    }
+    this.patientDialogVisible = false;
   }
 
   deletePatient(patient: IPatient) {
@@ -204,6 +241,8 @@ export class PatientManagementComponent implements OnInit, AfterViewInit {
                 summary: 'Success',
                 detail: 'Patient deleted successfully.',
               });
+
+              this.getPatients();
             },
             (error: any) => {
               this.messageService.add({
