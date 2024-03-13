6e75320fd8015d95bb52b3210aced7fbc4ecb074
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-03-13 20:10:00 +0200

    Refactor authentication.guard.ts file

--- src/app/core/authentication/authentication.guard.ts
+++ src/app/core/authentication/authentication.guard.ts
@@ -1,6 +1,6 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthenticationService } from './authentication.service';
+import { inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
+import { AuthenticationService } from "./authentication.service";
 
 export const authenticationGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
@@ -18,10 +18,25 @@ export const authenticationGuard: CanActivateFn = (route, state) => {
       // Redirect to /admin if trying to access other routes
       return router.createUrlTree(['/admin']);
     }
+  } else if (
+    authService.isAuthenticated() &&
+    authService.getRole().includes('practitioner')
+  ) {
+    // If user is authenticated and has 'practitioner' role
+    if (state.url.startsWith('/practitioner')) {
+      // Allow access to /practitioner routes
+      return true;
+    } else {
+      // Redirect to /practitioner if trying to access other routes
+      return router.createUrlTree(['/practitioner']);
+    }
   } else if (authService.isAuthenticated()) {
-    // If user is authenticated but doesn't have 'admin' role
-    if (state.url.startsWith('/admin')) {
-      // Redirect to /dashboard if trying to access /admin routes without 'admin' role
+    // If user is authenticated but doesn't have 'admin' or 'practitioner' role
+    if (
+      state.url.startsWith('/admin') ||
+      state.url.startsWith('/practitioner')
+    ) {
+      // Redirect to /dashboard if trying to access special routes without proper role
       return router.createUrlTree(['/dashboard']);
     } else {
       // Allow access to non-/admin routes
